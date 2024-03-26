aed0094e7ad5c817c9bcc22842fd109bfd18c1a3
Gonçalo Frazão <gonfrazao@gmail.com>
2024-03-26 01:02:40 +0000

    mpi checkerboard decomposition with masked communication

--- mpi-checkerboard/life3d-mpi.cc
+++ mpi-checkerboard/life3d-mpi.cc
@@ -1,23 +1,28 @@
-#include <iostream>
 #include <mpi.h>
 #include <omp.h>
+
 #include <cstring>	// memcpy function
 #include <iomanip>	// setprecision function
+#include <iostream>
+
 #include "grid.hh"
 
 #define N_SPECIES 9
 
 using namespace std;
 
-// Global variables to store the 
+// Global variables to store the
 int local_cells[N_SPECIES + 1];
 int max_cells[N_SPECIES + 1];
 int generation[N_SPECIES + 1];
 
+MPI_Datatype x_plane, y_plane, z_plane;
+MPI_Request send_x[2], send_y[2], send_z[2], recv_x[2], recv_y[2], recv_z[2];
+int _up, _down, _left, _right, _front, _back;
+int faces_recv;
 
 // Function to calculate the next state of a cell, based on its neighbors states
 char next_state(int x, int y, int z, char ***grid) {
-
 	// Initialize variables
 	static int ax, ay, az;
 	int sum = 0;
@@ -46,8 +51,8 @@ char next_state(int x, int y, int z, char ***grid) {
 		else
 			return (char)0;
 
-	// If current cell is dead
-	} else { 
+		// If current cell is dead
+	} else {
 		if (sum > 6 && sum < 11) {
 			int max = 1;
 			for (int i = 2; i <= N_SPECIES; i++)
@@ -94,9 +99,61 @@ void get_max(int *cells, int gen) {
 	}
 }
 
+int test_recv_data(MPI_Comm grid_comm, char ***new_grid, long long local_N[]) {
+	int flag;
+	switch (faces_recv) {
+		case 0:
+			faces_recv++;
+			MPI_Isend(&new_grid[1][1][1], 1, x_plane, _down, 1, grid_comm, send_x);
+			MPI_Isend(&new_grid[local_N[0]][1][1], 1, x_plane, _up, 0, grid_comm, send_x + 1);
+			break;
+
+		case 1:
+			MPI_Testall(2, recv_x, &flag, MPI_STATUSES_IGNORE);
+			if (flag) {
+				faces_recv++;  // faces_recv to 2 if x faces have been received
+				MPI_Isend(&new_grid[0][1][1], 1, y_plane, _left, 1, grid_comm, send_y);
+				MPI_Isend(&new_grid[0][local_N[1]][1], 1, y_plane, _right, 0, grid_comm, send_y + 1);
+			}
+			break;
+
+		case 2:
+			MPI_Testall(2, recv_y, &flag, MPI_STATUSES_IGNORE);
+			if (flag) {
+				faces_recv++;
+				MPI_Isend(&new_grid[0][0][1], 1, z_plane, _back, 1, grid_comm, send_z);
+				MPI_Isend(&new_grid[0][0][local_N[2]], 1, z_plane, _front, 0, grid_comm, send_z + 1);
+				return 1;
+			}
+			break;
+
+		case 4:
+			MPI_Waitall(2, send_x, MPI_STATUSES_IGNORE);
+			MPI_Waitall(2, send_y, MPI_STATUSES_IGNORE);
+			MPI_Waitall(2, send_z, MPI_STATUSES_IGNORE);
+			MPI_Waitall(2, recv_z, MPI_STATUSES_IGNORE);
+			return 1;
+			break;
+
+		default:
+			return 1;
+	}
+	return 0;
+}
+
+void recv_all(MPI_Comm grid_comm, char ***new_grid, long long local_N[]) {
+	MPI_Irecv(&new_grid[0][1][1], 1, x_plane, _down, 0, grid_comm, recv_x);
+	MPI_Irecv(&new_grid[local_N[0] + 1][1][1], 1, x_plane, _up, 1, grid_comm, recv_x + 1);
+
+	MPI_Irecv(&new_grid[0][0][1], 1, y_plane, _right, 0, grid_comm, recv_y);
+	MPI_Irecv(&new_grid[0][local_N[1] + 1][1], 1, y_plane, _left, 1, grid_comm, recv_y + 1);
+
+	MPI_Irecv(&new_grid[0][0][0], 1, z_plane, _back, 0, grid_comm, recv_z);
+	MPI_Irecv(&new_grid[0][0][local_N[2] + 1], 1, z_plane, _front, 1, grid_comm, recv_z + 1);
+}
+
 // Function to simulate the game of life
 void simulation(char ***grid, long long local_N[], int generations, int rank, MPI_Comm grid_comm) {
-
 	// Allocate memory for the 'next generation' grid, also with ghost cells
 	char ***new_grid = alloc_grid(local_N[0] + 2, local_N[1] + 2, local_N[2] + 2), ***temp;
 
@@ -109,18 +166,51 @@ void simulation(char ***grid, long long local_N[], int generations, int rank, MP
 
 	// Run the simulation for the specified number of generations
 	for (int i = 0; i < generations; i++) {
-
 		// Initialize the local cells array (also counts dead cells: N_SPECIES+1)
 		int cells[N_SPECIES + 1] = {0};
+		faces_recv = 0;
+
+		recv_all(grid_comm, new_grid, local_N);
+
+		for (int y = 1; y <= local_N[1]; y++) {
+			for (int z = 1; z <= local_N[2]; z++) {
+				new_grid[1][y][z] = next_state(1, y, z, grid);
+				cells[(int)new_grid[1][y][z]]++;
+				new_grid[local_N[0]][y][z] = next_state(local_N[0], y, z, grid);
+				cells[(int)new_grid[local_N[0]][y][z]]++;
+			}
+		}
+		test_recv_data(grid_comm, new_grid, local_N);
+
+		for (int x = 2; x < local_N[0]; x++) {
+			for (int z = 1; z <= local_N[2]; z++) {
+				new_grid[x][1][z] = next_state(x, 1, z, grid);
+				cells[(int)new_grid[x][1][z]]++;
+				new_grid[x][local_N[1]][z] = next_state(x, local_N[1], z, grid);
+				cells[(int)new_grid[x][local_N[1]][z]]++;
+			}
+		}
+		test_recv_data(grid_comm, new_grid, local_N);
+
+		for (int x = 2; x < local_N[0]; x++) {
+			for (int y = 2; y < local_N[1]; y++) {
+				new_grid[x][y][1] = next_state(x, y, 1, grid);
+				cells[(int)new_grid[x][y][1]]++;
+				new_grid[x][y][local_N[2]] = next_state(x, y, local_N[2], grid);
+				cells[(int)new_grid[x][y][local_N[2]]]++;
+			}
+		}
+		test_recv_data(grid_comm, new_grid, local_N);
 
 		// For each cell of the local grid, calculate the next state and update the local cells array
-		for (int x = 1; x <= local_N[0]; x++) {
-			for (int y = 1; y <= local_N[1]; y++) {
-				for (int z = 1; z <= local_N[2]; z++) {
+		for (int x = 2; x < local_N[0]; x++) {
+			for (int y = 2; y < local_N[1]; y++) {
+				for (int z = 2; z < local_N[2]; z++) {
 					new_grid[x][y][z] = next_state(x, y, z, grid);
 					cells[(int)new_grid[x][y][z]]++;
 				}
 			}
+			test_recv_data(grid_comm, new_grid, local_N);
 		}
 
 		// Reduce the local cells array counter to the root node
@@ -130,7 +220,11 @@ void simulation(char ***grid, long long local_N[], int generations, int rank, MP
 		if (!rank) get_max(local_cells, i + 1);
 
 		// Exchange the boundaries between the neighbor nodes in the 3D grid
-		exchange_boudaries(new_grid, local_N, grid_comm);
+		// exchange_boudaries(new_grid, local_N, grid_comm);
+		while (!test_recv_data(grid_comm, new_grid, local_N));
+
+		faces_recv++;
+		test_recv_data(grid_comm, new_grid, local_N);
 
 		// Swap the pointers of the grid and the new_grid
 		temp = grid;
@@ -139,37 +233,8 @@ void simulation(char ***grid, long long local_N[], int generations, int rank, MP
 	}
 }
 
-void print_grid(char ***grid, long long N, int id[], int p[]) {
-	MPI_Request reqs[4];
-	long long block_sizes[3] = {BLOCK_SIZE(id[0], p[0], N), BLOCK_SIZE(id[1], p[1], N), BLOCK_SIZE(id[2], p[2], N)};
-
-	int l[3] = {p[0] - 1, p[1] - 1, p[2] - 1};
-	int global_size = DIMS_TO_INDEX(l, p) + 1;
-
-	MPI_Barrier(MPI_COMM_WORLD);
-
-	int a = 1;
-	for (int i = 0; i < global_size; i++) {
-		if (DIMS_TO_INDEX(id, p) == i) {
-			cout << "Rank " << i << endl;
-			for (int x = 1 - a; x <= block_sizes[0] + a; x++) {
-				cout << "x = " << x + BLOCK_LOW(id[0], p[0], N) - 1 << endl;
-				for (int y = 1 - a; y <= block_sizes[1] + a; y++) {
-					for (int z = 1 - a; z <= block_sizes[2] + a; z++) {
-						cout << (int)grid[x][y][z] << " ";
-					}
-					cout << endl;
-				}
-				cout << endl;
-			}
-		}
-		MPI_Barrier(MPI_COMM_WORLD);
-	}
-}
-
-// Function to run the simulation using the checkerboard method 
+// Function to run the simulation using the checkerboard method
 void mpi_checkerboard_3d(int N, int generations, float density, int seed) {
-
 	// Store the node rank and the total number of nodes
 	int rank, p;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -182,12 +247,23 @@ void mpi_checkerboard_3d(int N, int generations, float density, int seed) {
 	MPI_Dims_create(p, 3, dims);
 	MPI_Cart_create(MPI_COMM_WORLD, 3, dims, periods, 0, &grid_comm);
 
-	// Get the coordinates of the current node in the 3D grid	
+	// Get the coordinates of the current node in the 3D grid
 	int coords[3];
 	MPI_Cart_coords(grid_comm, rank, 3, coords);
 
 	// Calculate the size of the local grid for the current node (3D block)
 	long long local_N[3] = {BLOCK_SIZE(coords[0], dims[0], N), BLOCK_SIZE(coords[1], dims[1], N), BLOCK_SIZE(coords[2], dims[2], N)};
+
+	MPI_Cart_shift(grid_comm, 0, 1, &_down, &_up);
+	MPI_Cart_shift(grid_comm, 1, 1, &_left, &_right);
+	MPI_Cart_shift(grid_comm, 2, 1, &_back, &_front);
+
+	MPI_Type_vector(local_N[1], local_N[2], local_N[2] + 2, MPI_CHAR, &x_plane);
+	MPI_Type_vector(local_N[0] + 2, local_N[2], (local_N[1] + 2) * (local_N[2] + 2), MPI_CHAR, &y_plane);
+	MPI_Type_vector((local_N[0] + 2) * (local_N[1] + 2), 1, local_N[2] + 2, MPI_CHAR, &z_plane);
+	MPI_Type_commit(&x_plane);
+	MPI_Type_commit(&y_plane);
+	MPI_Type_commit(&z_plane);
 
 	// Generate the initial grid
 	char ***grid = gen_initial_grid(N, density, seed, coords, dims);
@@ -209,12 +285,14 @@ void mpi_checkerboard_3d(int N, int generations, float density, int seed) {
 
 	// Print the results of the simulation (only by the master node)
 	if (!rank) print_result(grid, N);
-}
 
+	MPI_Type_free(&x_plane);
+	MPI_Type_free(&y_plane);
+	MPI_Type_free(&z_plane);
+}
 
 /* MAIN PROGRAM */
 int main(int argc, char *argv[]) {
-
 	// Check if the input is valid
 	if (argc != 5 || atoi(argv[1]) <= 0 || atoi(argv[2]) <= 0 || atof(argv[3]) < 0 || atof(argv[3]) > 1) {
 		cerr << "Usage: " << argv[0] << " <generations> <N> <density> <seed>\n";
